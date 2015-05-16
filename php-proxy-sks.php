06019ed6ac18f001777226baa9888ef1f6720040
Carles Tubio <ctubio@users.noreply.github.com>
2015-05-16 12:25:41 -0700

    Added error handler for pks requests.

--- php-proxy-sks.php
+++ php-proxy-sks.php
@@ -1,18 +1,20 @@
 <?php
 use Proxy\Factory;
-use Proxy\Response\Filter\RemoveEncodingFilter;
 use Symfony\Component\HttpFoundation\Request;
 
 require 'vendor/autoload.php';
 
-// Create a Symfony request based on the current browser request.
 $request = Request::createFromGlobals();
 
-// Forward the request and get the response.
 $response = Factory::forward($request)->to(
   'http://'.$request->server->get('SERVER_ADDR').':11371'
   .$request->server->get('REQUEST_URI')
 );
 
-// Output response to the browser.
-$response->send();
+if (
+ $response->getStatusCode()!='200'
+ and is_readable($response->getStatusCode().'.html')
+)
+  readfile($response->getStatusCode().'.html');
+else
+  $response->send();
