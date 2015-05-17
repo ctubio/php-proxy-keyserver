520c933bf54e1c25138fb634e66dcce7d6b63230
Carles Tubio <ctubio@users.noreply.github.com>
2015-05-17 05:05:28 +0200

    Updated config file.

--- pub/php-proxy-sks.php
+++ pub/php-proxy-sks.php
@@ -6,19 +6,17 @@ require '../vendor/autoload.php';
 
 $request = Request::createFromGlobals();
 
-$config = parse_ini_file(realpath('../etc/php-proxy-sks.ini'));
 $config = (object)array_merge(
   array(
     'hkp_port' => '11371',
-    'hkp_address' => $request->server->get('SERVER_ADDR')
+    'hkp_addr' => $request->server->get('SERVER_ADDR')
   ),
-  parse_ini_file($config['sksconf'])
+  parse_ini_file(realpath('../etc/php-proxy-sks.ini'))
 );
-$config->hkp_address = current(explode(' ', $config->hkp_address));
 
 if (!isset($_GET['errno']))
   $response = Factory::forward($request)->to(
-    'http://'.$config->hkp_address.':'.$config->hkp_port
+    'http://'.$config->hkp_addr.':'.$config->hkp_port
     .$request->server->get('REQUEST_URI')
   );
 
