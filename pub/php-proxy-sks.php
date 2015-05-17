4b555c678415c3402507a09e1e86a8410c95da0e
Carles Tubio <ctubio@users.noreply.github.com>
2015-05-17 04:52:13 +0200

    Added config file.

--- pub/php-proxy-sks.php
+++ pub/php-proxy-sks.php
@@ -6,20 +6,31 @@ require '../vendor/autoload.php';
 
 $request = Request::createFromGlobals();
 
-$response = Factory::forward($request)->to(
-  'http://'.$request->server->get('SERVER_ADDR').':11371'
-  .$request->server->get('REQUEST_URI')
+$config = parse_ini_file(realpath('../etc/php-proxy-sks.ini'));
+$config = (object)array_merge(
+  array(
+    'hkp_port' => '11371',
+    'hkp_address' => $request->server->get('SERVER_ADDR')
+  ),
+  parse_ini_file($config['sksconf'])
 );
+$config->hkp_address = current(explode(' ', $config->hkp_address));
 
+if (!isset($_GET['errno']))
+  $response = Factory::forward($request)->to(
+    'http://'.$config->hkp_address.':'.$config->hkp_port
+    .$request->server->get('REQUEST_URI')
+  );
 
 if (
- ($statusCode = $response->getStatusCode()) !== '200'
- and is_numeric($statusCode)
- and is_readable($statusCode.'.html')
+ ($errno = isset($_GET['errno'])
+   ? $_GET['errno'] : $response->getStatusCode()
+ ) !== '200'
+ and is_numeric($errno)
+ and is_readable($error = realpath($errno.'.html'))
 ) {
-  $response->setContent(file_get_contents(
-    $statusCode.'.html'
-  ));
+  $response->setContent(file_get_contents($error));
 }
 
 $response->send();
+
