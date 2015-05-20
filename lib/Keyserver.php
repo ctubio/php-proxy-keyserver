6bf8813cfd4a4a466b36171ef86967ded1350687
Carles Tubio <ctubio@users.noreply.github.com>
2015-05-20 20:05:30 +0200

    Fixed outdated options in Log.

--- lib/Keyserver.php
+++ lib/Keyserver.php
@@ -5,17 +5,21 @@ use PhpProxySks\Keyserver\Router;
 use Symfony\Component\HttpFoundation\Request;
 
 class Keyserver {
-  
+
   public static $request_instance;
-  
+
   public static function getRequest() {
     if (self::$request_instance === NULL)
       self::$request_instance = Request::createFromGlobals();
     return self::$request_instance;
   }
 
-  public static function getErrno() {
-    return (int)self::getRequest()->query->get('ERRNO');
+  public static function getResponse() {
+    return Router::getResponse();
+  }
+
+  public static function getConfig() {
+    return Config::getInstance();
   }
 
   public static function getUri() {
@@ -23,11 +27,8 @@ class Keyserver {
       self::getRequest()->server->get('REQUEST_URI')
     );
   }
-  public static function getConfig() {
-    return Config::getInstance();
-  }
 
-  public static function getResponse() {
-    return Router::getResponse();
+  public static function getErrno() {
+    return (int)self::getRequest()->query->get('ERRNO');
   }
 }
--- lib/Keyserver/Log.php
+++ lib/Keyserver/Log.php
@@ -5,15 +5,15 @@ use PhpProxySks\Keyserver;
 class Log {
 
   public static function catchError($e) {
-    $error = $e->getMessage().PHP_EOL.$e->getTraceAsString();
+    $e = $e->getMessage().PHP_EOL.$e->getTraceAsString();
 
-    if ((bool)(int)Keyserver::getConfig()->debug_mode)
-      die('<pre>'.$error.'</pre>');
+    if ((bool)(int)Keyserver::getConfig()->display_errors)
+      die('<pre>'.$e.'</pre>');
 
     if (!$path=realpath('../log'))
       @mkdir($path=realpath('..').'/log',0777,TRUE);
     $path .= '/php-proxy-sks.log';
-    @file_put_contents($path, $error.PHP_EOL
+    @file_put_contents($path, $e.PHP_EOL
       .(file_exists($path)?@file_get_contents($path):NULL)
     );
   }
