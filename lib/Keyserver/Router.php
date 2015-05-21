3c5454bf95f75236ae414d983f2785920f805400
Carles Tubio <ctubio@users.noreply.github.com>
2015-05-21 14:06:02 +0200

    Updated Router with detailed methods.

--- lib/Keyserver/Router.php
+++ lib/Keyserver/Router.php
@@ -8,24 +8,28 @@ use Symfony\Component\HttpFoundation\Response;
 class Router {
 
   public static function getResponse() {
-    $config = Keyserver::getConfig();
-
-    try {
-      $response = ($errno = Keyserver::getErrno())
-        ? new Response(NULL, $errno)
-        : (strpos($uri = Keyserver::getUri(), '/pks/') === 0
-            ? Skin::parseContent(Factory::forward(Keyserver::getRequest())->to(
-                'http://'.$config->hkp_addr.':'.$config->hkp_port.$uri
-              ))
-            : Skin::parsePhtml(new Response(), $uri)
-          );
-    } catch (\Exception $e) {
-      Log::catchError($e, 'Double-check if the keyserver is up and running at the expected address:port ('.$config->hkp_addr.':'.$config->hkp_port.').');
-    }
+    $response = ($errno = Keyserver::getErrno())
+      ? new Response(NULL, $errno)
+      : (strpos($uri = Keyserver::getUri(), '/pks/') === 0
+          ? Skin::parseContent(self::_getHKPResponse($uri))
+          : Skin::parsePhtml(new Response(), $uri)
+        );
 
     if (($errno = ($errno ?: (int)$response->getStatusCode())) !== 200)
       $response = Skin::parsePhtml($response, '/errors/'.$errno);
 
     return $response;
+  }
+
+  public static function _getHKPResponse($uri) {
+    $config = Keyserver::getConfig();
+
+    try {
+      return Factory::forward(Keyserver::getRequest())->to(
+        'http://'.$config->hkp_addr.':'.$config->hkp_port.$uri
+      );
+    } catch (\Exception $e) {
+      Log::catchError($e, 'Double-check if the keyserver is up and running at the expected address:port ('.$config->hkp_addr.':'.$config->hkp_port.').');
+    }
   }
 }
