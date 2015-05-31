f635cdbf480c5a5d4c5a079d7ebc89430cf79a41
Carles Tubio <ctubio@users.noreply.github.com>
2015-05-31 19:17:56 +0200

    Update sks-dump-alert.sh

--- bin/sks-dump-alert.sh
+++ bin/sks-dump-alert.sh
@@ -3,7 +3,7 @@ MAIL="carles.tubio@key-server.io"
 THRESHOLD="80"
 PARTITION="/dev/hdv1"
 
-if (($(df -h / | grep "${PARTITION}" | awk '{ print $5 }' | sed 's/%//g') > ${THRESHOLD})); then
+if (($(df -h / | grep "${PARTITION}" | awk '{ print $5 }' | sed 's/%//g') >= ${THRESHOLD})); then
   echo "${PARTITION} at $(hostname -f) reached ${THRESHOLD}%" | \
     mail -s "${PARTITION} at $(hostname -f) reached ${THRESHOLD}%" "${MAIL}"
 fi;
