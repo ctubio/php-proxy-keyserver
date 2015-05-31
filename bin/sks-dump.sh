7480661b03a58ff3df3adc2a8b0e3b76a281255c
Carles Tubio <ctubio@users.noreply.github.com>
2015-06-01 01:30:50 +0200

    Update sks-dump.sh

--- bin/sks-dump.sh
+++ bin/sks-dump.sh
@@ -54,17 +54,23 @@ The current archive size is approximately $SIZE, holding $DCOUNT keys in $FILES 
 
 On a linux/unix system, you may download this directory via the following command:
 
-  $ cd /var/lib/sks/dump
+  $ cd /var/lib/sks
+  $ rm -rf dump
+  $ mkdir dump
+  $ cd dump
   $ wget -c -r -p -e robots=off --timestamping --level=1 --cut-dirs=3 --no-host-directories https://${HOSTNAME}/dump/current/
 
-After downloading the dump files, you can import them into a new empty dabatase with the following command:
+After downloading the dump files, you can import them into a new database with the following command:
 
   $ cd /var/lib/sks
+  $ rm -rf KDB PTree
   $ /usr/local/bin/sks_build.sh
 
-and choose the option 2.
+and choose the option 2 (normalbuild).
 
 If all goes smoothly you'll end up with KDB and PTree directories in /var/lib/sks, and you are ready to start the daemons.
+
+The content of /var/lib/sks/dump directory can be removed, and additionally, can be replaced by daily dumps of your own database.
 
 Also, if you would like to peer with this server, please send an email to <${CONTACT}> with your membership line." > $OUTDIR/README.txt;
 
