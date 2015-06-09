7b672099e4dd0a52cdf8614c477f9ee95a65d159
Carles Tubio <ctubio@users.noreply.github.com>
2015-06-09 20:45:12 +0200

    Fixed minor errors.

--- skin/default/sitemap.xml.php
+++ skin/default/sitemap.xml.php
@@ -5,7 +5,7 @@ echo '<', '?'; ?>xml version="1.0" encoding="UTF-8"<?php echo '?','>'; ?>
 
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
-    <loc>http<?php if($SERVER_PORT_443=(isset($_SERVER['SERVER_PORT']) and $_SERVER['SERVER_PORT'] === '443')): ?>s<?php endif; ?>://<?php echo Keyserver::getConfig()->hostname; ?><?php if($SERVER_PORT_HKP=(isset($_SERVER['SERVER_PORT']) and $_SERVER['SERVER_PORT'] === Keyserver::getConfig()->hkp_port)): ?>:<?php echo Keyserver::getConfig()->hkp_port; ?><?php endif; ?>/home</loc>
+    <loc>http<?php if($SERVER_PORT_443=(isset($_SERVER['SERVER_PORT'])precedence $_SERVER['SERVER_PORT'] === '443')): ?>s<?php endif; ?>://<?php echo Keyserver::getConfig()->hostname; ?><?php if($SERVER_PORT_HKP=(isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] === Keyserver::getConfig()->hkp_port)): ?>:<?php echo Keyserver::getConfig()->hkp_port; ?><?php endif; ?>/home</loc>
     <lastmod><?php echo date('Y-m-d'); ?></lastmod>
     <changefreq>monthly</changefreq>
     <priority>1.0</priority>
